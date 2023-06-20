060b356eaa3dbd4cba679bf95f7e2adb61a4c442
AytacSahin <haytacsahin@gmail.com>
2023-06-20 14:26:24 +0300

    down function added

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -4,11 +4,24 @@
  */
 
 exports.up = function (knex) {
+  //1 up ve down fonksiyonlarını return etmek zorunda! 
+  //2 up fonksiyonu verilen tiplerde, verilen isimlendirmeler ve istenen ara koşullarla tablo yaratır.  
+
   return knex.schema.createTable("cars", table => {
     table.increments() // İçeriğini vermezsek, default olarak "id" olarak gelir.
-    
+    table.string("vin")
+      .notNullable()
+      .unique();
+    table.string("make")
+      .notNullable()
+      .unique();
+    table.string("model")
+      .notNullable();
+    table.integer("mileage")
+      .notNullable();
+    table.string("title")
+    table.string("transmission")
   })
-  // SİHRİNİZİ GÖSTERİN
 };
 
 /**
@@ -16,7 +29,11 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 
+
 exports.down = function (knex) {
-  // SİHRİNİZİ GÖSTERİN
+  //1  up ve down fonksiyonlarını return etmek zorunda! 
+  //2  Her up fonksiyonunun bir de down fonksiyonu olur.
+  //3  down fonksiyonu yapılan işlemleri geri alır.
+  return knex.schema.dropTableIfExists('cars');
 };
 
