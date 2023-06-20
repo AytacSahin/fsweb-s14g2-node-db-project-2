6c902b277a869b1334fdd87a318820e184d0caa4
AytacSahin <haytacsahin@gmail.com>
2023-06-20 14:23:33 +0300

    up function added

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
 exports.up = function (knex) {
+  return knex.schema.createTable("cars", table => {
+    table.increments() // İçeriğini vermezsek, default olarak "id" olarak gelir.
+    
+  })
   // SİHRİNİZİ GÖSTERİN
 };
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
 
 exports.down = function (knex) {
   // SİHRİNİZİ GÖSTERİN
 };
+
